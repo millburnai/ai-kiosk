5ebdd3f6eaf2363e502684fbe9d0eaeb7be27579
Liam <22pilarskil@millburn.org>
2019-12-30 17:43:27 -0500

    implementing dropbox

--- dataflow/simpleparse.py
+++ dataflow/simpleparse.py
@@ -1,52 +1,60 @@
+if __name__ == "__main__":
 
-import os
-import json
+    import os
+    import json
+    import argparse
+    import functools
+    import os
+    import shutil
+    import re
+    import warnings
 
-def embed(img_dir, dump_path, error_path, verify=False):
-    dict_list = {}
-    x = 0
-    for dir in os.listdir(img_dir):
-        print(dir)
-        if os.path.isdir(os.path.join(img_dir, dir)):
-            print("dumdum")
-            encrypted_data, no_faces = dump_embeds(facenet, os.path.join(img_dir, dir), dump_path,
-                        full_overwrite=True, mode=("w+" if x == 0 else "a+"), ignore_encrypt="all")
-            dict_list.update(encrypted_data)
+    from aisecurity import FaceNet
+    from aisecurity.data.dataflow import dump_embeds, retrieve_embeds
+    from aisecurity.utils.paths import CONFIG_HOME
+    from aisecurity.privacy.encryptions import *
 
-            print("oops")
-            x+=1
+    facenet = FaceNet(CONFIG_HOME + "/models/ms_celeb_1m.h5")
 
-    print(os.listdir(img_dir))
-    with open(dump_path, 'w') as json_file:
-        json.dump(dict_list, json_file, indent=4, ensure_ascii=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_dir", help="path to image directory", type=str)
+    parser.add_argument("--dump_path", help="path to JSON file", type=str)
+    parser.add_argument("--verify", help="run facial recognition with new data", type=to_bool, default=False)
 
-    if verify: 
-        verify_data(dump_path)
+    def verify_data(dump_path):
+        print("Running facial recognition with new data to verify")
+        facenet.set_data(retrieve_embeds(dump_path, encrypted=None))
+        input("Verify data: press enter")
+        facenet.real_time_recognize(logging=None, pre_recorded_file='/Users/michaelpilarski/Desktop/test_movie.mov')
 
-def verify_data(dump_path):
-    print("Running facial recognition with new data to verify")
-    facenet.set_data(retrieve_embeds(dump_path, encrypted=None))
-    input("ISO: Press Enter")
-    facenet.real_time_recognize(logging=None, pre_recorded_file='/Users/michaelpilarski/Desktop/movie.mov', False)
+    def send_to_dropbox():
+        input("Send items? ({}, {}, {}".format(args.dump_path, CONFIG_HOME+"/keys/embedding_keys.txt", CONFIG_HOME+"/keys/name_keys.txt"))
+        os.chdir(CONFIG_HOME+"/bin")
+        os.system('sh dump_embeds.sh {} "/aisecurity/database/embeddings(12/30/31).txt" {}'.format(args.dropbox_key, args.dump_path))
+        os.system('sh dump_embeds.sh {} "/aisecurity/keys/embedding_keys(12/30/31).txt {}'.format(args.dropbox_key, CONFIG_HOME+"/keys/embedding_keys.txt"))
+        os.system('sh dump_embeds.sh {} "/aisecurity/keys/name_keys(12/30/31).txt {}'.format(args.dropbox_key, CONFIG_HOME+"/keys/name_keys.txt"))
 
-import argparse
-import functools
-import os
-import shutil
-import re
-import warnings
+    def embed(img_dir, dump_path, verify=False):
+        dict_list = {}
+        x = 0
+        for dir in os.listdir(img_dir):
+            print(dir)
+            if os.path.isdir(os.path.join(img_dir, dir)):
+                print("dumdum")
+                encrypted_data, no_faces = dump_embeds(facenet, os.path.join(img_dir, dir), dump_path,
+                            full_overwrite=True, mode=("w+" if x == 0 else "a+"), ignore_encrypt="all")
+                dict_list.update(encrypted_data)
+                print("oops")
+                x+=1
 
-from aisecurity import FaceNet
-from aisecurity.data.dataflow import dump_embeds, retrieve_embeds
-from aisecurity.utils.paths import CONFIG_HOME
-from aisecurity.privacy.encryptions import *
+        print(os.listdir(img_dir))
+        with open(dump_path, 'w+') as json_file:
+            json.dump(dict_list, json_file, indent=4, ensure_ascii=False)
 
-facenet = FaceNet(CONFIG_HOME + "/models/ms_celeb_1m.h5")
+        if verify: 
+            verify_data(dump_path)
 
+        send_to_dropbox()
 
-print(os.getcwd())
-os.chdir(flash_drive)
+    embed(args.img_dir, args.dump_path, args.verify)
 
-embed("parsed_images", "embeddings.txt", "missed_frames.txt", True)
-
-verify_data("embeddings.txt")
