825e30b097f2f40967ea2ffb85ad7044ecd24c5e
Ethan Slepner <56898342+Harp-Seal@users.noreply.github.com>
2020-01-14 14:58:21 -0500

    Update tensorflow.sh

--- scripts/tensorflow.sh
+++ scripts/tensorflow.sh
@@ -3,4 +3,5 @@ sudo apt-get install python3-pip
 sudo python3 -m pip install -U pip --user
 sudo python3 -m pip install -U numpy grpcio absl-py py-cpuinfo psutil portpicker six mock requests gast h5py astor termcolor protobuf keras-applications keras-preprocessing wrapt google-pasta --user
 sudo python3 -m pip install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14.0+nv19.9 --user
-
+sudo apt-get install git-all
+sudo python3 -m pip install "git+https://github.com/orangese/easyai"
