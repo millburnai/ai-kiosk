a03e821655fdf01e6dc06e02916b42c60c9e4084
jetson nano <22parkr@millburn.org>
2020-02-20 15:04:47 -0500

    Use  in pycuda_install.sh

--- scripts/install/pycuda_install.sh
+++ scripts/install/pycuda_install.sh
@@ -5,13 +5,20 @@
 # sudo apt-get install build-essential python-dev python-setuptools libboost-python-dev libboost-thread-dev -y
 # sudo python3 -m pip install numpy
 
-wget -O "pycuda-VERSION.tar.gz" https://files.pythonhosted.org/packages/5e/3f/5658c38579b41866ba21ee1b5020b8225cec86fe717e4b1c5c972de0a33c/pycuda-2019.1.2.tar.gz
-if [ ! -d pycuda-VERSION ] ; then
-    mkdir pycuda-VERSION && tar zxvf pycuda-VERSION.tar.gz -C pycuda-VERSION --strip-components 1
+TMP=$HOME/temp_dir
+mkdir $TMP
+cd $TMP
+
+if [ ! -f $TMP/pycuda-VERSION.tar.gz ] ; then
+    wget -O "pycuda-VERSION.tar.gz" https://files.pythonhosted.org/packages/5e/3f/5658c38579b41866ba21ee1b5020b8225cec86fe717e4b1c5c972de0a33c/pycuda-2019.1.2.tar.gz
+fi
+if [ ! -d $TMP/pycuda-VERSION ] ; then
+    cd $TMP
+    tar xpvf pycuda-VERSION.tar.gz
 fi
 
-cd pycuda-VERSION
+cd $TMP/pycuda-VERSION
 python3 configure.py --cuda-root=/usr/local/cuda --cudadrv-lib-dir=/usr/lib --boost-inc-dir=/usr/include --boost-lib-dir=/usr/lib --boost-python-libname=boost_python-py36 --boost-thread-libname=boost_thread
-make -j 4
+make -j ${nproc}
 sudo -H python3 setup.py install
 sudo -H python3 -m pip install .
