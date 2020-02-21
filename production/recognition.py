57912a7088963dd0a4ca53b5a7b842fc6563f10e
jetson nano <22parkr@millburn.org>
2020-02-21 08:39:51 -0500

    Change params in production/recognition.py

--- production/recognition.py
+++ production/recognition.py
@@ -6,7 +6,7 @@ Production facial recognition script. Called on startup by /etc/rc.local
 
 import signal
 
-from aisecurity import FaceNet, DEFAULT_MODEL
+from aisecurity import FaceNet
 
 
 def recognize(facenet, sec):
@@ -15,7 +15,7 @@ def recognize(facenet, sec):
         raise Exception("{}s have elapsed, killing process".format(sec))
 
     def _recognize(facenet):
-        facenet.real_time_recognize(use_picam=False, flip=0, data_mutability=0, use_graphics=True)
+        facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=0, use_graphics=False)
 
     signal.signal(signal.SIGALRM, handler)
     signal.alarm(sec)
@@ -26,5 +26,5 @@ def recognize(facenet, sec):
         print(e)
 
 if __name__ == "__main__":
-    facenet = FaceNet(DEFAULT_MODEL.replace("pb", "engine"))
-    recognize(facenet, sec=20)
+    facenet = FaceNet()
+    recognize(facenet, sec=60)
