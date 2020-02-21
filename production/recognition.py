05fe6d1a12d3b670fe99fe250ef38f902b73c538
jetson nano <22parkr@millburn.org>
2020-02-20 20:54:35 -0500

    Remove recognition.sh and update recognition.py

--- production/recognition.py
+++ production/recognition.py
@@ -7,4 +7,4 @@ Will be filled in later once I retrieve the code from the Jetson Nano.
 import aisecurity
 
 facenet = aisecurity.FaceNet("/home/aisecurity/.aisecurity/models/ms_celeb_1m.engine")
-facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=1, use_graphics=False)
+facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=0, use_graphics=False)
--- production/recognition.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/python3 /home/aisecurity/ai-kiosk/production/recognition.sh && exit 0
