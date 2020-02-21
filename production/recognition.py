31be3617d382ca9fc321c9f54ddd22f6e070ae6f
jetson nano <22parkr@millburn.org>
2020-02-20 21:20:31 -0500

    Add mp timeout to recognition.py

--- production/recognition.py
+++ production/recognition.py
@@ -1,10 +1,34 @@
 """
 
-Will be filled in later once I retrieve the code from the Jetson Nano.
+Production facial recognition script. Called on startup by /etc/rc.local
 
 """
 
+import functools
+import multiprocessing
+
 import aisecurity
 
-facenet = aisecurity.FaceNet("/home/aisecurity/.aisecurity/models/ms_celeb_1m.engine")
-facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=0, use_graphics=False)
+def timeout(sec):
+    def _timeout(func):
+        @functools.wraps(func)
+        def _func(*args, **kwargs):
+            p = multiprocessing.Process(target=bar, args=args, kwargs=kwargs)
+            p.start()
+
+            p.join(sec)
+
+            if p.is_alive():
+                print("Killing func after {} seconds".format(sec))
+
+                p.terminate()
+                p.join()
+
+@timeout(sec=10)
+def recognize(facenet):
+    facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=0, use_graphics=False)
+
+
+if __name__ == "__main__":
+    facenet = aisecurity.FaceNet("/home/aisecurity/.aisecurity/models/ms_celeb_1m.engine")
+    recognize(facenet)
