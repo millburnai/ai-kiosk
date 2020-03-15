8e8580b9250312d6739ff1cb512507bc68ccc3f2
22pilarskil <22pilarskil@millburn.org>
2020-03-14 20:16:05 -0400

    Local changes

--- production/recognition.py
+++ production/recognition.py
@@ -15,7 +15,7 @@ def recognize(facenet, sec):
         raise Exception("{}s have elapsed, killing process".format(sec))
 
     def _recognize(facenet):
-        facenet.real_time_recognize(use_picam=True, flip=0, data_mutability=0, use_lcd=True, use_graphics=False)
+        facenet.real_time_recognize(use_picam=True, socket=True, flip=0, data_mutability=True, use_graphics=True)
 
     signal.signal(signal.SIGALRM, handler)
     signal.alarm(sec)
@@ -28,3 +28,4 @@ def recognize(facenet, sec):
 if __name__ == "__main__":
     facenet = FaceNet()
     recognize(facenet, sec=100)
+    #facenet.real_time_recognize(use_picam=True, socket=True, flip=0, data_mutability=0, use_graphics=True)
